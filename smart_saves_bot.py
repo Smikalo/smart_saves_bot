c863fc8a8c5aba3a473f944042b3445f82989703
Mikhail Kozyrev <smrtkozyrev@gmail.com>
2022-02-05 01:31:23 +0300

    added basic echo parsing

--- smart_saves_bot.py
+++ smart_saves_bot.py
@@ -4,3 +4,11 @@ from telebot import types
 from linkpreview import link_preview
 
 bot=telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(content_types=['text'])
+def link_preview_parsing(message):
+    if message.chat.type=='private' and (message.text[:8]=='https://' or message.text[:7]=='http://'):
+        bot.send_message(message.chat.id, link_preview(message.text).description)
+    else:
+        bot.send_message(message.chat.id, '0')
+bot.polling(none_stop=True)
