dd5a2a220119eede737473874086dd70be922f1c
Mikhail Kozyrev <smrtkozyrev@gmail.com>
2022-02-06 18:44:27 +0300

    Bot handles links now. UPD: smart_saves_bot.py, ToDo.txt

--- smart_saves_bot.py
+++ smart_saves_bot.py
@@ -1,14 +1,37 @@
 import telebot
+import linkpreview
 import config
 from telebot import types
 from linkpreview import link_preview
 
 bot=telebot.TeleBot(config.TOKEN)
 
+tags_db={
+    'coding':['python', 'programmer'],
+    'physics':['physics', 'einstein']
+}
+
 @bot.message_handler(content_types=['text'])
 def link_preview_parsing(message):
-    if message.chat.type=='private' and (message.text[:8]=='https://' or message.text[:7]=='http://'):
-        bot.send_message(message.chat.id, link_preview(message.text).description)
-    else:
-        bot.send_message(message.chat.id, '0')
+    try:
+        text=message.text
+        link_data=link_preview(message.text)
+        if message.chat.type=='private' and (text[:8]=='https://' or text[:7]=='http://') and text.count('\n')==0:
+            title=link_data.title
+            description=link_data.description
+            
+            data=(title+'\n'+description).lower()
+            
+            correct_tags=[(i*(sum([(j in data) for j in tags_db[i]]) > 0)) for i in tags_db]
+            if '' in correct_tags:
+                correct_tags.remove('')
+            
+            tags = '#'+' #'.join(correct_tags)
+
+            bot.send_message(message.chat.id, text+'\n'+tags)
+        else:
+            bot.send_message(message.chat.id, 'Message is not a single link.')
+    except linkpreview.exceptions.MaximumContentSizeError as e:
+        bot.send_message(message.chat.id, 'Link is unreadable for some reason. Please tag it manually.')
+
 bot.polling(none_stop=True)
